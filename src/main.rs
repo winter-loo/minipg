c0b1320e95a75175077cd5ca07fae7cebd78afaa
winter.loo <adjkldd@126.com>
2024-01-30 11:19:35 +0800

    find ok

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-const MAX_KEYS: usize = 8;
+const MAX_CHILDREN: usize = 5;
+const INIT: Option<Box<Node>> = None;
 
 #[derive(Debug)]
 struct Node {
     n: usize,
-    keys: [usize; MAX_KEYS],
-    children: Vec<Node>,
+    keys: [usize; MAX_CHILDREN - 1],
+    children: [Option<Box<Node>>; MAX_CHILDREN],
     is_leaf: bool,
 }
 
@@ -12,8 +13,8 @@ impl Node {
     fn new() -> Node {
         Node {
             n: 0,
-            keys: [0; MAX_KEYS],
-            children: Vec::new(),
+            keys: [0; MAX_CHILDREN - 1],
+            children: [INIT; MAX_CHILDREN],
             is_leaf: false,
         }
     }
@@ -24,29 +25,37 @@ impl Node {
             i += 1;
         }
         if i < self.n && key == self.keys[i] {
-            return Some(&self.children[i]);
+            return Some(self);
         }
         if self.is_leaf {
             return None;
         }
-        self.children[i].find(key)
+        self.children[i].as_ref().unwrap().find(key)
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_find() {
         let mut root = Node::new();
+        root.is_leaf = true;
         root.keys[0] = 10;
-        root.children.push(Node::new());
-        assert_eq!(root.find(20).unwrap().n, 20);
+        root.n += 1;
+        assert!(root.find(20).is_none());
+
+        root.keys[1] = 20;
+        root.n += 1;
+        let it = root.find(20);
+        assert!(it.is_some());
+        assert_eq!(it.unwrap().keys[1], 20); 
     }
 }
 
 fn main() {
-    let mut root = Node::new(10);
-    root.children.push(Node::new(20));
+    let mut root = Node::new();
+    root.keys[0] = 10;
     println!("{:?}", root);
 }
